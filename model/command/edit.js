282356070d05539d4dba24038de2b11383234dac
Elian Wonhalf <elianwonhalf@gmail.com>
2019-12-31 20:37:55 -0500

    Added command edit

---
+++ model/command/edit.js
@@ -0,0 +1,30 @@
+const Logger = require('@elian-wonhalf/pretty-logger');
+const Guild = require('../guild');
+
+/**
+ * @param {Message} message
+ */
+module.exports = {
+    aliases: ['send'],
+    process: async (message, content) => {
+        const member = await Guild.getMemberFromMessage(message);
+
+        if (Guild.isMemberMod(member)) {
+            let channel = message.channel;
+
+            if (message.mentions.channels.size > 0) {
+                channel = message.mentions.channels.first();
+                content.shift();
+            }
+
+            const target = await channel.fetchMessage(content.shift());
+
+            target.edit(content.join(' ')).then(async () => {
+                await message.react(bot.emojis.find(emoji => emoji.name === 'pollyes'));
+            }).catch(async (error) => {
+                Logger.error(error.toString());
+                await message.react(bot.emojis.find(emoji => emoji.name === 'pollno'));
+            });
+        }
+    }
+};
