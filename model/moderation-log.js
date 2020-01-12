0f1d3143a05beaab8f06b50522273201c6d3d649
Elian Wonhalf <elianwonhalf@gmail.com>
2020-01-12 01:45:26 -0500

    Fixed public mod log

--- model/moderation-log.js
+++ model/moderation-log.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const GuildAuditLogs = Discord.GuildAuditLogs;
 const Config = require('../config.json');
 const Guild = require('./guild');
 
@@ -6,129 +7,86 @@ const ModerationLog = {
     memberLeftDate: null,
     auditLogFetchInterval: 5 * 60 * 1000, // 5 minutes
     searchAuditLogTimeout: null,
-    membersWhoLeft: {},
     lastFetchedAuditLogId: null,
     language: Config.botLanguage.split(',')[0],
 
-    processMemberRemove: (user, forceLogNow) => {
-        forceLogNow = forceLogNow || false;
+    processMemberRemove: async (user, banned) => {
+        banned = banned || false;
 
-        const nowDate = new Date();
-        const nowTime = nowDate.getTime();
-        const lastLogDateIsNull = ModerationLog.memberLeftDate === null;
-        const elapsedTimeSinceLastLog = lastLogDateIsNull ? 0 : nowTime - ModerationLog.memberLeftDate.getTime();
-        const canLogNow = lastLogDateIsNull || elapsedTimeSinceLastLog >= ModerationLog.auditLogFetchInterval;
-
-        ModerationLog.membersWhoLeft[user.id] = null;
         debug(`Detected member leaving: ${user.username}`);
 
-        if (!forceLogNow && !canLogNow && ModerationLog.searchAuditLogTimeout === null) {
-            debug('Cannot log now, and no timeout set; setting a timeout');
-            ModerationLog.searchAuditLogTimeout = setTimeout(
-                ModerationLog.doLog,
-                ModerationLog.auditLogFetchInterval - elapsedTimeSinceLastLog
-            );
-        } else if (canLogNow || forceLogNow) {
-            debug('Can log now');
+        const auditLogs = await Guild.discordGuild.fetchAuditLogs({
+            after: ModerationLog.lastFetchedAuditLogId,
+            type: banned ? GuildAuditLogs.Actions.MEMBER_BAN_ADD : GuildAuditLogs.Actions.MEMBER_KICK,
+            limit: 1
+        });
 
-            clearTimeout(ModerationLog.searchAuditLogTimeout);
-            ModerationLog.doLog();
-        } else {
-            debug('Cannot log now, and a timeout is set');
-        }
-
-        ModerationLog.memberLeftDate = nowDate;
-    },
-
-    doLog: async () => {
-        const memberIds = Object.keys(ModerationLog.membersWhoLeft);
-
-        ModerationLog.searchAuditLogTimeout = null;
-
-        debug(`Starting moderation logging; memberIds.length = ${memberIds.length}`);
-        if (memberIds.length > 0) {
-            const auditLogs = await Guild.discordGuild.fetchAuditLogs({
-                after: ModerationLog.lastFetchedAuditLogId,
-            });
-
+        if (auditLogs.entries.size > 0) {
             ModerationLog.lastFetchedAuditLogId = auditLogs.entries.first().id;
             debug(`${auditLogs.entries.size} entr${auditLogs.entries.size > 1 ? 'ies' : 'y'} in the audit log before filtering`);
 
             const entries = auditLogs.entries.filter(entry => {
                 const userTarget = entry.targetType === 'USER';
-                const kickOrBan = entry.action === 'MEMBER_KICK' || entry.action === 'MEMBER_BAN_ADD';
-                const inList = entry.target !== undefined && memberIds.indexOf(entry.target.id) > -1;
+                const isTarget = entry.target !== undefined && user.id === entry.target.id;
                 const notAuto = entry.reason === null || entry.reason.indexOf('[AUTO]') < 0;
 
-                if (inList && (!userTarget || !kickOrBan || !notAuto)) {
+                if (isTarget && (!userTarget || !notAuto)) {
                     debug(`Removed entry corresponding to user`);
 
                     debug(`userTarget: ${userTarget ? 'true' : 'false'}`);
                     debug(`entry.targetType: ${entry.targetType}`);
 
-                    debug(`kickOrBan: ${kickOrBan ? 'true' : 'false'}`);
-                    debug(`entry.action: ${entry.action}`);
-
                     debug(`notAuto: ${notAuto ? 'true' : 'false'}`);
                     debug(`entry.reason: ${entry.reason}`);
+
                     if (entry.reason !== null) {
                         debug(`entry.reason.indexOf('[AUTO]'): ${entry.reason.indexOf('[AUTO]')}`);
                     }
                 }
 
-                return userTarget && kickOrBan && inList && notAuto;
+                return userTarget && isTarget && notAuto;
             });
 
             debug(`${entries.size} entr${auditLogs.entries.size > 1 ? 'ies' : 'y'} in the audit log after filtering`);
 
-            entries.forEach(entry => {
-                if (ModerationLog.membersWhoLeft[entry.target.id] === null) {
-                    const member = trans(
-                        'model.moderationLog.member',
-                        [`<@${entry.target.id}>`],
-                        ModerationLog.language
-                    );
+            entries.forEach(async entry => {
+                const member = trans(
+                    'model.moderationLog.member',
+                    [`<@${entry.target.id}>`],
+                    ModerationLog.language
+                );
 
-                    const embed = new Discord.RichEmbed().setAuthor(
-                        `${entry.target.username}#${entry.target.discriminator}`,
-                        entry.target.displayAvatarURL
-                    );
+                const embed = new Discord.RichEmbed().setAuthor(
+                    `${entry.target.username}#${entry.target.discriminator}`,
+                    entry.target.displayAvatarURL
+                );
 
-                    let action = '';
-                    let reason = '';
+                let action = '';
+                let reason = '';
 
-                    switch (entry.action) {
-                        case 'MEMBER_KICK':
-                            action = trans('model.moderationLog.kicked', [], ModerationLog.language);
-                            embed.setColor(0xFC8403); // orange
-                            break;
+                switch (entry.action) {
+                    case 'MEMBER_KICK':
+                        action = trans('model.moderationLog.kicked', [], ModerationLog.language);
+                        embed.setColor(0xFC8403); // orange
+                        break;
 
-                        case 'MEMBER_BAN_ADD':
-                            action = trans('model.moderationLog.banned', [], ModerationLog.language);
-                            embed.setColor(0xFF0000); // red
-                            break;
-                    }
-
-                    if (entry.reason !== null) {
-                        reason = entry.reason.replace(/https?:\/\/[^\s.]+\.[^\s]+/g, '[CENSORED LINK]');
-                        reason = trans('model.moderationLog.reason', [reason], ModerationLog.language);
-                    } else {
-                        Guild.modLogChannel.send(trans('model.moderationLog.missingReason'));
-                    }
-
-                    embed.setDescription(`${member} ${action} ${reason}`);
-                    ModerationLog.membersWhoLeft[entry.target.id] = embed;
+                    case 'MEMBER_BAN_ADD':
+                        action = trans('model.moderationLog.banned', [], ModerationLog.language);
+                        embed.setColor(0xFF0000); // red
+                        break;
                 }
+
+                if (entry.reason !== null) {
+                    reason = entry.reason.replace(/https?:\/\/[^\s.]+\.[^\s]+/g, '[CENSORED LINK]');
+                    reason = trans('model.moderationLog.reason', [reason], ModerationLog.language);
+                } else {
+                    await Guild.modLogChannel.send(trans('model.moderationLog.missingReason'));
+                }
+
+                embed.setDescription(`${member} ${action} ${reason}`);
+                await Guild.publicModLogChannel.send(embed);
             });
-
-            for (const memberId in ModerationLog.membersWhoLeft) {
-                if (ModerationLog.membersWhoLeft.hasOwnProperty(memberId) && ModerationLog.membersWhoLeft[memberId] !== null) {
-                    Guild.publicModLogChannel.send(ModerationLog.membersWhoLeft[memberId]);
-                }
-            }
         }
-
-        ModerationLog.membersWhoLeft = {};
     }
 };
 
