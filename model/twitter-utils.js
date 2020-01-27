76ed4376a504d56588b67c983b07cdeeb420cf23
Elian Wonhalf <elianwonhalf@gmail.com>
2020-01-26 21:31:29 -0500

    Fixed twitter fatal error

--- model/twitter-utils.js
+++ model/twitter-utils.js
@@ -4,34 +4,40 @@ const Config = require('../config.json');
 const Guild = require('./guild');
 const MAX_CHAR_COUNT = 280;
 
-const T = new Twit({
-    consumer_key: Config.socialMedia.twitter.apiKey,
-    consumer_secret: Config.socialMedia.twitter.apiSecretKey,
-    access_token: Config.socialMedia.twitter.accessToken,
-    access_token_secret: Config.socialMedia.twitter.accessTokenSecret
-});
+let T = null;
+
+if (Config.socialMedia.twitter.apiKey !== '') {
+    T = new Twit({
+        consumer_key: Config.socialMedia.twitter.apiKey,
+        consumer_secret: Config.socialMedia.twitter.apiSecretKey,
+        access_token: Config.socialMedia.twitter.accessToken,
+        access_token_secret: Config.socialMedia.twitter.accessTokenSecret
+    });
+}
 
 const TwitterUtils = {
     postMessage: async (message) => {
-        const member = await Guild.getMemberFromMessage(message);
-        const messageLength = MAX_CHAR_COUNT - member.displayName.length - 45;
-        const truncatedMessage = message.cleanContent.substr(0, messageLength);
-        const status = member.displayName + ' sur Discord :\n'
-            + truncatedMessage + (truncatedMessage.length !== message.cleanContent.length ? '...' : '')
-            + '\n\nhttps://discord.gg/french';
+        if (Config.socialMedia.twitter.apiKey !== '') {
+            const member = await Guild.getMemberFromMessage(message);
+            const messageLength = MAX_CHAR_COUNT - member.displayName.length - 45;
+            const truncatedMessage = message.cleanContent.substr(0, messageLength);
+            const status = member.displayName + ' sur Discord :\n'
+                + truncatedMessage + (truncatedMessage.length !== message.cleanContent.length ? '...' : '')
+                + '\n\nhttps://discord.gg/french';
 
-        Logger.info('Publishing post on Twitter...');
+            Logger.info('Publishing post on Twitter...');
 
-        T.post(
-            'statuses/update',
-            { status: status }
-        ).then(async () => {
-            Logger.info('✅ Successfully published on Twitter!');
-            await message.react(bot.emojis.find(emoji => emoji.name === 'pollyes'));
-        }).catch(async error => {
-            Logger.error(error.body);
-            await message.react(bot.emojis.find(emoji => emoji.name === 'pollno'));
-        });
+            T.post(
+                'statuses/update',
+                {status: status}
+            ).then(async () => {
+                Logger.info('✅ Successfully published on Twitter!');
+                await message.react(bot.emojis.find(emoji => emoji.name === 'pollyes'));
+            }).catch(async error => {
+                Logger.error(error.body);
+                await message.react(bot.emojis.find(emoji => emoji.name === 'pollno'));
+            });
+        }
     }
 };
 
