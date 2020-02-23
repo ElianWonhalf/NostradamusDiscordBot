3169900f3c8eb1236686062a88e2582cf05a1968
Elian Wonhalf <elianwonhalf@gmail.com>
2020-02-22 19:00:22 -0500

    Bugfixes

--- model/twitter-utils.js
+++ model/twitter-utils.js
@@ -1,4 +1,5 @@
 const Twit = require('twit');
+const got = require('got');
 const Logger = require('@elian-wonhalf/pretty-logger');
 const Config = require('../config.json');
 const Guild = require('./guild');
@@ -16,28 +17,68 @@ if (Config.socialMedia.twitter.apiKey !== '') {
 }
 
 const TwitterUtils = {
-    postMessage: async (message) => {
+    /**
+     * @param {Message} message
+     * @param {String} content
+     * @param {String} image
+     * @returns {Promise.<void>}
+     */
+    postMessage: async (message, content, image) => {
         if (Config.socialMedia.twitter.apiKey !== '') {
+            image = image || null;
+
             const member = await Guild.getMemberFromMessage(message);
             const messageLength = MAX_CHAR_COUNT - member.displayName.length - 45;
-            const truncatedMessage = message.cleanContent.substr(0, messageLength);
+            const truncatedMessage = content.substr(0, messageLength);
             const status = member.displayName + ' sur Discord :\n'
-                + truncatedMessage + (truncatedMessage.length !== message.cleanContent.length ? '...' : '')
+                + truncatedMessage + (truncatedMessage.length !== content.length ? '...' : '')
                 + '\n\nhttps://discord.gg/french';
 
             Logger.info('Publishing post on Twitter...');
 
-            T.post(
-                'statuses/update',
-                {status: status}
-            ).then(async () => {
+            const postPromise = image === null
+                ? TwitterUtils.postTextStatus(status)
+                : TwitterUtils.postImageStatus(status, image);
+
+            postPromise.then(async () => {
                 Logger.info('✅ Successfully published on Twitter!');
                 await message.react(bot.emojis.find(emoji => emoji.name === 'pollyes'));
             }).catch(async error => {
-                Logger.error(error.body);
+                Logger.exception(error);
                 await message.react(bot.emojis.find(emoji => emoji.name === 'pollno'));
             });
         }
+    },
+
+    postTextStatus: (content) => {
+        return T.post(
+            'statuses/update',
+            {status: content}
+        );
+    },
+
+    /**
+     * @param {String} content
+     * @param {String} image
+     * @returns {Promise.<void>}
+     */
+    postImageStatus: (content, image) => {
+        return new Promise(async (resolve, reject) => {
+            const response = await got(image, {responseType: 'buffer', encoding: null});
+            const imageBase64 = response.body.toString('base64');
+
+            // First we must post the media to Twitter
+            T.post('media/upload', {media_data: imageBase64}, function (err, data) {
+                if (!err) { // Can't have a strict comparison here 😿
+                    const mediaIdStr = data.media_id_string;
+                    const params = {status: content, media_ids: [mediaIdStr]};
+
+                    T.post('statuses/update', params).then(resolve).catch(reject);
+                } else {
+                    reject(err);
+                }
+            });
+        });
     }
 };
 
