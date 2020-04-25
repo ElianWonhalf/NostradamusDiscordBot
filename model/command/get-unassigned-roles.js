6efa988f400c6a90ef577297dcb95d5bd1bee354
Elian Wonhalf <elianwonhalf@gmail.com>
2020-04-25 15:05:06 -0400

    Fixed get-unassigned-roles command

--- model/command/get-unassigned-roles.js
+++ model/command/get-unassigned-roles.js
@@ -1,3 +1,4 @@
+const Discord = require('discord.js');
 const Guild = require('../guild');
 const CommandCategory = require('../command-category');
 const CommandPermission = require('../command-permission');
@@ -7,13 +8,20 @@ const CommandPermission = require('../command-permission');
  */
 module.exports = {
     aliases: ['getunassignedroles', 'get-unassigned-role', 'getunassignedrole', 'gur'],
-    category: CommandCategory.BOT_MANAGEMENT,
+    category: CommandCategory.ADMINISTRATION,
     isAllowedForContext: CommandPermission.isMommy,
     process: async (message) => {
+        const waitMessage = await message.reply(trans('model.command.getUnassignedRoles.wait', [], 'en'));
+
         const unassignedRoles = Guild.discordGuild.roles.cache.filter(role => {
-            return Guild.discordGuild.members.cache.filter(member => member.roles.cache.has(role.id)).size > 0;
+            return Guild.discordGuild.members.cache.filter(member => member.roles.cache.has(role.id)).size < 1;
         }).map(role => role.name).join('\n');
 
-        message.channel.send(unassignedRoles);
+        const answers = Discord.Util.splitMessage(unassignedRoles);
+        await waitMessage.delete();
+
+        for (let i = 0; i < answers.length; i++) {
+            await message.channel.send(answers[i]);
+        }
     }
 };
