505b91e2a882964187e4edda4ae1b164069ada39
Lily Wonhalf <lilywonhalf@gmail.com>
2020-06-13 12:26:57 -0400

    Added watch log for when member leaves

--- event/bot/guildMemberRemove.js
+++ event/bot/guildMemberRemove.js
@@ -1,5 +1,6 @@
 const ModerationLog = require('../../model/moderation-log');
 const Guild = require('../../model/guild');
+const WatchedMember = require('../../model/watched-member');
 const StatMemberFlow = require('../../model/stat-member-flow');
 
 /**
@@ -9,6 +10,7 @@ module.exports = async (member) => {
     if (isRightGuild(member.guild.id)) {
         member = await bot.users.fetch(member.id);
 
+        WatchedMember.guildMemberAddHandler(member);
         StatMemberFlow.save(member.id, StatMemberFlow.constructor.MEMBER_FLOW_EVENT_LEFT);
         Guild.stopMemberReactionCollectors(member.id);
         ModerationLog.processMemberRemove(member);
