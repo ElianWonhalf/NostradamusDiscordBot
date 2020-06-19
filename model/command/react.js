cb0f4706443718971e22e597bc1492256bb15f8c
Lily Wonhalf <lilywonhalf@gmail.com>
2020-06-18 23:18:52 -0400

    Added command react

--- model/command/react.js
+++ model/command/react.js
@@ -2,17 +2,43 @@ const Logger = require('@lilywonhalf/pretty-logger');
 const CommandCategory = require('../command-category');
 const CommandPermission = require('../command-permission');
 
+const emojiNameRegex = /^<?[^:]*:(?<name>[^:]+):[^>]*>?$/u;
+
 /**
  * @param {Message} message
  */
 module.exports = {
     aliases: [],
-    category: CommandCategory.BOT_MANAGEMENT,
+    category: CommandCategory.RESOURCE,
     isAllowedForContext: CommandPermission.isMemberMod,
-    process: async (message) => {
-        const emoji = bot.emojis.cache.find(emoji => emoji.name === 'eowynsheep');
+    process: async (message, args) => {
+        const messageId = args.shift();
+        const targetedMessage = await message.channel.messages.fetch(messageId);
 
-        await message.react(emoji);
-        Logger.notice('killnostrapls');
+        const emojis = args.map(arg => {
+            let foundEmoji = arg.replace(/[\s\n]+/u, '');
+
+            if (foundEmoji.match(emojiNameRegex) !== null) {
+                const name = arg.replace(emojiNameRegex, '$<name>');
+                foundEmoji = bot.emojis.cache.find(emoji => emoji.name === name);
+
+                if (typeof foundEmoji === 'undefined') {
+                    message.reply(trans('model.command.react.notFound', [name], 'en'));
+                }
+            }
+
+            return foundEmoji;
+        }).filter(emoji => typeof emoji !== 'undefined');
+
+        while (emojis.length > 0) {
+            const emoji = emojis.shift();
+
+            await targetedMessage.react(emoji).catch(error => {
+                Logger.error(error);
+                message.reply(trans('model.command.react.error', [emoji.name, error.trace], 'en'));
+            });
+        }
+
+        await message.delete();
     }
 };
