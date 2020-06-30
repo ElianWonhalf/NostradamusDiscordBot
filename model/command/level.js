0f20d9beb1b73a81ee0fb0423db2fe5f86c03813
SpécialFlocon <specialflocon@protonmail.com>
2020-06-30 12:00:27 +0200

    Implement suggestions, round 2

--- model/command/level.js
+++ model/command/level.js
@@ -21,6 +21,7 @@ class Level
      * @param {Array} args
      */
     async process(message, args) {
+        const author = await Guild.getMemberFromMessage(message);
         const argsStr = args.join(' ').toLowerCase().trim();
 
         if (!Guild.levelRoles.some(roleName => argsStr.includes(roleName.toLowerCase()))) {
@@ -44,11 +45,12 @@ class Level
         const targetRole = Guild.getRoleByName(targetRoleName);
         const levelRoles = Guild.levelRoles.map(name => Guild.getRoleByName(name));
 
-        message.mentions.members.each(member => {
+        foundMembers.each(member => {
             await Promise.all([
                 member.roles.remove(levelRoles),
                 member.roles.add(targetRole),
             ]);
+            Guild.serverLogChannel.send(trans('model.command.level.logAction', [author.id, member.id, targetRole]));
         });
 
         message.reply(trans('model.command.level.setRole', [targetRoleName]));
