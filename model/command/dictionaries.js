aefa07060982485ac8e54b23773dce46b70912ac
Lily Wonhalf <lilywonhalf@gmail.com>
2020-08-05 02:21:47 -0400

    Fixed syntax, added embed

--- model/command/dictionaries.js
+++ model/command/dictionaries.js
@@ -1,3 +1,4 @@
+const { MessageEmbed } = require('discord.js');
 const Heat = require('../heat');
 const CommandCategory = require('../command-category');
 const CommandPermission = require('../command-permission');
@@ -12,7 +13,7 @@ class Dictionaries extends Heat
         }
 
         super(10 * SECOND);
-        this.aliases = ['dictionnaires','dico','traducteurs','translators'];
+        this.aliases = ['dictionnaires', 'dico', 'traducteurs', 'translators'];
         this.category = CommandCategory.RESOURCE;
         this.isAllowedForContext = CommandPermission.notInWelcome;
     }
@@ -23,17 +24,20 @@ class Dictionaries extends Heat
     async process(message) {
         if (this.canCall()) {
             this.registerCall();
+            const embed = new MessageEmbed();
+            const description = trans(
+                'model.command.dictionaries.reply',
+                [
+                    ['model.command.dictionaries.reply.wordreference'],
+                    ['model.command.dictionaries.reply.usito'],
+                    ['model.command.dictionaries.reply.deepl'],
+                    ['model.command.dictionaries.reply.googleTranslate'],
+                    ['model.command.dictionaries.reply.reversoContextAndLinguee'],
+                ]
+            );
+
             message.channel.send(
-                trans(
-                    'model.command.dictionaries.reply',
-                    [
-                        ['model.command.dictionaries.reply.wordreference'],
-                        ['model.command.dictionaries.reply.usito'],
-                        ['model.command.dictionaries.reply.deepl'],
-                        ['model.command.dictionaries.reply.googleTranslate'],
-                        ['model.command.dictionaries.reply.reversoContextAndLinguee'],
-                    ]
-                )
+                embed.setDescription(description).setColor(0x00FF00)
             );
         } else {
             message.react('⌛');
