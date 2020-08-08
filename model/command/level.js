54606f771408a4e630a5e5fe2ba187b525467796
Lily Wonhalf <lilywonhalf@gmail.com>
2020-08-08 01:24:45 -0400

    Removed mod ping from level command

--- model/command/level.js
+++ model/command/level.js
@@ -52,7 +52,7 @@ class Level
             await member.roles.remove(levelRoles);
             await member.roles.add(targetRoleId);
 
-            Guild.botChannel.send(trans('model.command.level.logAction', [author.toString(), member.id, targetRole.name], 'en'));
+            Guild.botChannel.send(trans('model.command.level.logAction', [author.username, member.id, targetRole.name], 'en'));
         });
 
         message.reply(trans('model.command.level.setRole', [targetRole.name]));
