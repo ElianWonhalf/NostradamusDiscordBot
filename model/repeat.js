f68c834e6553d2c32054993635acc80439567b6b
Lily Wonhalf <lilywonhalf@gmail.com>
2020-09-14 16:02:32 -0400

    Fixed repeat

--- model/repeat.js
+++ model/repeat.js
@@ -13,10 +13,12 @@ class Repeat {
 
         if (lastMessage.has(message.channel.id)) {
             const { lastString, snowflakes, answered } = lastMessage.get(message.channel.id);
-            const hasMentions = message.mentions.guild.size > 0
-                || message.mentions.roles.size > 0
-                || message.mentions.everyone
-                || message.mentions.users.size > 0;
+            const shouldRepeat = message.mentions.guild.size < 1
+                && message.mentions.roles.size < 1
+                && !message.mentions.everyone
+                && message.mentions.users.size < 1
+                && !/^[\W_]/u.test(message.cleanContent)
+                && !/http/gu.test(message.cleanContent);
 
             if (message.cleanContent === lastString && !answered && !snowflakes.includes(message.author.id)) {
                 snowflakes.push(message.author.id);
@@ -27,7 +29,7 @@ class Repeat {
                 );
             }
 
-            if (snowflakes.length > 2 && !answered && !hasMentions) {
+            if (snowflakes.length > 2 && !answered && shouldRepeat) {
                 message.channel.send(message.cleanContent);
                 lastMessage.set(
                     message.channel.id,
