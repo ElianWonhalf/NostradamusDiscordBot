5fbbbeca26a0e3c1141afdd0c3b85bfd36fe7fa1
SpécialFlocon <specialflocon@protonmail.com>
2020-10-14 21:42:56 +0200

    Return 5 first results of the gmid command search

--- model/command/get-member-id.js
+++ model/command/get-member-id.js
@@ -1,3 +1,4 @@
+const Discord = require('discord.js');
 const Guild = require('../guild');
 const CommandCategory = require('../command-category');
 const CommandPermission = require('../command-permission');
@@ -23,8 +24,13 @@ class GetMemberId
         const result = Guild.findDesignatedMemberInMessage(message);
 
         if (result.foundMembers.length > 0) {
-            message.channel.send(result.foundMembers[0].toString());
-            message.channel.send(result.foundMembers[0].id);
+            result.foundMembers.slice(0, 5).forEach(async member => {
+                const embed = new Discord.MessageEmbed().setAuthor(
+                    `${member.user.username}#${member.user.discriminator}`,
+                    member.user.displayAvatarURL({ dynamic: true })
+                ).setDescription(`ID: ${member.id}`);
+                await message.channel.send(embed);
+            });
         } else {
             message.reply(trans('model.command.getMemberId.notFound', [], 'en'));
         }
