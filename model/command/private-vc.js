a32289f35f211067a43ab75b74aaa62b1f839630
SpécialFlocon <specialflocon@protonmail.com>
2021-01-09 00:14:14 +0100

    Reply with a message if a regular member uses the .private-vc command

--- model/command/private-vc.js
+++ model/command/private-vc.js
@@ -1,5 +1,6 @@
 const CommandCategory = require('../command-category');
 const CommandPermission = require('../command-permission');
+const Guild = require('../guild');
 const PrivateVCModel = require('../private-vc');
 
 class PrivateVC
@@ -13,7 +14,7 @@ class PrivateVC
 
         this.aliases = ['privatevc', 'pvc'];
         this.category = CommandCategory.MODERATION;
-        this.isAllowedForContext = CommandPermission.isMemberModOrSoft;
+        this.isAllowedForContext = CommandPermission.notInWelcome;
     }
 
     /**
@@ -21,32 +22,36 @@ class PrivateVC
      * @param {Array} args
      */
     async process(message, args) {
-        if (args.length > 0) {
-            const action = args.shift().toLowerCase();
-            const emoji = bot.emojis.cache.find(emoji => emoji.name === 'pollyes');
+        if (Guild.isMemberMod(message.member) || Guild.isMemberSoft(message.member)) {
+            if (args.length > 0) {
+                const action = args.shift().toLowerCase();
+                const emoji = bot.emojis.cache.find(emoji => emoji.name === 'pollyes');
 
-            switch (action) {
-                case 'lock':
-                case 'close':
-                    PrivateVCModel.lockRequestChannel();
-                    break;
+                switch (action) {
+                    case 'lock':
+                    case 'close':
+                        PrivateVCModel.lockRequestChannel();
+                        break;
 
-                case 'unlock':
-                case 'open':
-                    PrivateVCModel.shutdown = false;
-                    PrivateVCModel.unlockRequestChannel();
-                    break;
+                    case 'unlock':
+                    case 'open':
+                        PrivateVCModel.shutdown = false;
+                        PrivateVCModel.unlockRequestChannel();
+                        break;
 
-                case 'shutdown':
-                    PrivateVCModel.emergencyShutdown();
+                    case 'shutdown':
+                        PrivateVCModel.emergencyShutdown();
 
-                default:
-                    message.reply(trans('model.command.privateVC.error.unknownAction', [action], 'en'));
+                    default:
+                        message.reply(trans('model.command.privateVC.error.unknownAction', [action], 'en'));
+                }
+
+                await message.react(emoji);
+            } else {
+                message.reply(trans('model.command.privateVC.error.missingAction', [], 'en'));
             }
-
-            await message.react(emoji);
         } else {
-            message.reply(trans('model.command.privateVC.error.missingAction', [], 'en'));
+            message.reply(trans('model.command.privateVC.error.misused', [Guild.smallVoiceChatRequestChannel.name]));
         }
     }
 }
