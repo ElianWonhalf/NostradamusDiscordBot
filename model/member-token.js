b2a0fe8fecac0277943a70f3b5594176f4060a8f
Julien Chemin <you@example.com>
2021-02-02 15:59:26 +0100

    Commande "token-board"

--- model/member-token.js
+++ model/member-token.js
@@ -17,6 +17,22 @@ const MemberToken = {
             snowflakes
         );
     },
+
+    /**
+     * @param {String<Snowflake>|null} snowflake
+     * @returns {Promise}
+     */
+    getCount: async (snowflake = null) => {
+        await db.asyncQuery('SET NAMES utf8mb4');
+
+        const clauseWHERE = snowflake === null ? "" : `WHERE user_id = (?) `;
+        const arraySnowflake = [snowflake];
+
+        return await db.asyncQuery(
+            `SELECT COUNT(*) AS amount_token, user_id FROM member_token ${clauseWHERE}GROUP BY user_id ORDER BY amount_token DESC`,
+            arraySnowflake
+        );
+    },
 }
 
 module.exports = MemberToken;
