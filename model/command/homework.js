dfe79f25744cd425b0c6ad0e8cb943c88498e4ad
SpécialFlocon <specialflocon@protonmail.com>
2021-04-25 16:21:40 +0200

    Randomly send a fancy illustration to the homework rule

--- model/command/homework.js
+++ model/command/homework.js
@@ -29,15 +29,24 @@ class Homework extends Heat
             this.registerCall();
             message.delete();
 
-            const embed = new MessageEmbed()
-                .setDescription(
-                    `${emojiFrench} **French version**
-                    ${trans('model.command.homework.reply', [emojiFrench], 'fr')}
-                    ${emojiAnglophonie} **English version**
-                    ${trans('model.command.homework.reply', [emojiAnglophonie], 'en')}
-                    [Cliquez ici pour en savoir plus / Click here to know more](https://discord.com/channels/254463427949494292/557006102483435543/832654588326445056)`
-                );
-            message.channel.send(embed);
+            if (Math.random() < 0.09) {
+                message.channel.send({
+                    files: [{
+                        attachment: 'static/images/homework-rule.png',
+                        name: 'homework-rule.png'
+                    }]
+                })
+            } else {
+                const embed = new MessageEmbed()
+                    .setDescription(
+                        `${emojiFrench} **French version**
+                        ${trans('model.command.homework.reply', [emojiFrench], 'fr')}
+                        ${emojiAnglophonie} **English version**
+                        ${trans('model.command.homework.reply', [emojiAnglophonie], 'en')}
+                        [Cliquez ici pour en savoir plus / Click here to know more](https://discord.com/channels/254463427949494292/557006102483435543/832654588326445056)`
+                    );
+                message.channel.send(embed);
+            }
         } else {
             message.react('⌛');
         }
