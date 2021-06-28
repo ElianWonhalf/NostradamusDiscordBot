d5d8d1d95e36c0debbdc4f29b066d8bab7c0e803
Lily Wonhalf <lilywonhalf@gmail.com>
2021-06-28 05:28:10 -0400

    Fixed config files

--- model/guild.js
+++ model/guild.js
@@ -351,7 +351,7 @@ const Guild = {
      * @param {GuildMember} member
      */
     isMemberTokenMaster: (member) => {
-        return member !== undefined && member !== null && member.roles.cache.has(Config.roles.tokenMaster);
+        return member !== undefined && member !== null && member.roles.cache.has(Config.roles.tokenMastery);
     },
 
     /**
